7f9cfa41c025927d8fc7a877bdc7fa9bfac10c1e
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-05 15:14:03 +0100

    exercise done (palindrome)

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,1 +1,23 @@
-alert('sono collegato')
+/* 1 */
+
+let insertWord = prompt('Inserisci una parola:');
+
+let word = insertWord.toLowerCase();
+
+/* 2 */
+
+function isPalindrome(word){
+    /* trasformo la stringa in un array */
+    let splitWord = word.split("");
+    /* inverto l'ordine delle lettere all'interno dell'array */
+    let reverseArray = splitWord.reverse();
+    /* trasformo l'array nuovamente in stringa */
+    let reverseWord = reverseArray.join("");
+    if (reverseWord == word){
+        return `La parola '${word}' è palindroma`
+    } else {
+        return `La parola '${word}' non è palindroma`
+    }
+}
+
+alert(isPalindrome(word));
